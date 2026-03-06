37e06aae75de67279d0ef9f4053888d9f57070a8
binalamimtiaz@gmail.com <binalamimtiaz@gmail.com>
2026-03-06 20:18:34 +0600

    running

---
+++ practiceWeb/script.js
@@ -0,0 +1,19 @@
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const firstname = document.getElementById("fname").value;
+    const lastname = document.getElementById("lname").value;    
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const message = document.getElementById("message").value;
+
+    if(!firstname || !lastname || !email || !phone || !message) {
+        alert("Please fill in all fields.");
+        return;
+    }   
+
+    console.log("First Name:", firstname);  
+    
+
+    
+});
