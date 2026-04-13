5bbacc4950701974e3664499662d2b912ff141a2
binalamimtiaz@gmail.com <binalamimtiaz@gmail.com>
2026-04-13 21:19:59 +0600

    class work done

--- class-work/controller/indexValidation.php
+++ class-work/controller/indexValidation.php
@@ -17,5 +17,23 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     if(!empty($name) && strlen($name)>5){
         echo "User Name: ".$name;
     }
+    else{
+        $name = "User Name is required and must be more than 5 characters";
+    }
+
+    if(!empty($password) && strlen($password)>8){
+        $validpassword = $password;
+        echo " Password: ".$validpassword;
+    }
+    else{
+        $validpassword = "Password is required and must be more than 8 characters";
+    }   
+
+    if(!empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL)){
+        echo " Email: ".$email;
+    }
+    else{
+        $email = "Email is required and must be a valid email address";
+    }
 }
 ?>
--- class-work/view/index.php
+++ class-work/view/index.php
@@ -1,3 +1,7 @@
+<?php 
+include "../controller/indexValidation.php";    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
