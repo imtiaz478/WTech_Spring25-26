8f3fb267ee42071cca370dda0caf07253c5b23ae
binalamimtiaz@gmail.com <binalamimtiaz@gmail.com>
2026-04-13 21:11:04 +0600

    frontend done

--- class-work/view/index.php
+++ class-work/view/index.php
@@ -15,6 +15,20 @@
                 <td><label for="UserName">User Name:</label></td>
                 <td><input type="text" id="name" name="name"><?php echo $name; ?></td>
                 <td><p style='color: red'>*</p></td>
+
+            </tr>
+            <tr>
+                <td><label for="email">Email: </label></td>
+                <td><input type="email" id="email" name="email"><?php echo $email ?></td>
+            </tr>
+
+            <tr>
+                <td><label for="password">Password: </label></td>
+                <td><input type="password" id="password" name="password"><?php echo $password ?></td>   
+            </tr>
+
+            <tr>
+                <td><input type="submit" id="submit" name="submit" value="Submit"></td>
             </tr>
         </table>
     </form>
