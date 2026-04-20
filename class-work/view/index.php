5026819661656bb1ddd17ad72ea5f0ac18c99cd6
binalamimtiaz@gmail.com <binalamimtiaz@gmail.com>
2026-04-20 13:56:43 +0600

    running

--- class-work/view/index.php
+++ class-work/view/index.php
@@ -11,6 +11,19 @@ include "../controller/indexValidation.php";
    
 </head>
 <body>
+<p class="required-note">* required field</p>
+<form method="post" action="">
+        <table>
+        <tr>
+                <td><label for="name">Name:</label></td>
+                <td><input type="text" id="name" name="name"></td>
+                <td><span class="star">*</span> <?php echo isset($name) ? $name : ''; ?></td>
+            </tr>
+
+           
+        </table>
+    </form>
+
    
 </body>
 </html>
