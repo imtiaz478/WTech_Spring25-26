8d3c37f699a432965758fddc287b167bcfae324b
binalamimtiaz@gmail.com <binalamimtiaz@gmail.com>
2026-04-21 11:26:00 +0600

    done

--- class-work/controller/indexValidation.php
+++ class-work/controller/indexValidation.php
@@ -38,32 +38,17 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         echo "Website: ".$website;
     }   
 
-    
+    $comment = $_POST["comment"];   
 
-    $name = $_REQUEST["name"];
-    $password = $_REQUEST["password"];  
-    $email = $_REQUEST["email"];
-
-    if(!empty($name) && strlen($name)>5){
-        echo "User Name: ".$name;
+    if(empty($_POST["gender"])){
+        $gender = "Gender is required"; 
     }
     else{
-        $name = "User Name is required and must be more than 5 characters";
+        $gender = "";
     }
 
-    if(!empty($password) && strlen($password)>8){
-        $validpassword = $password;
-        echo " Password: ".$validpassword;
-    }
-    else{
-        $validpassword = "Password is required and must be more than 8 characters";
-    }   
 
-    if(!empty($email) && filter_var($email, FILTER_VALIDATE_EMAIL)){
-        echo " Email: ".$email;
-    }
-    else{
-        $email = "Email is required and must be a valid email address";
-    }
+
+   
 }
 ?>
