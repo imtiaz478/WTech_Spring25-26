260a4e0892aba9df6529aa3a44199145282649a5
binalamimtiaz@gmail.com <binalamimtiaz@gmail.com>
2026-04-21 11:22:38 +0600

    validation fixing

--- class-work/controller/indexValidation.php
+++ class-work/controller/indexValidation.php
@@ -1,14 +1,44 @@
 <?php
 
 $name ="";
-$password ="";
-$validpassword = "";
 $email = "";
+$website = "";
+$comment ="";
+$gender=""; 
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     $name = $_POST["name"];
-    $password = $_POST["password"]; 
+
+    if(empty($name)){
+        $name = "Name is required";
+    }
+    elseif(!preg_match("/^[a-zA-Z ]+$/", $name)){
+        $name = "Only letters and white space allowed";
+    }
+    else{
+        echo "User Name: ".$name;
+    }
+   
     $email = $_POST["email"];
+    if(empty($email)){
+        $email = "E-mail is required";
+    }
+    elseif(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $email = "Invalid email format";
+    }
+    else{
+        echo "Email: ".$email;  
+    }
+
+    $website = $_POST["website"];   
+    if(!empty($website) && !filter_var($website, FILTER_VALIDATE_URL)){
+        $website = "Invalid URL";
+    }
+    else{
+        echo "Website: ".$website;
+    }   
+
+    
 
     $name = $_REQUEST["name"];
     $password = $_REQUEST["password"];  
